f64e4e41d28c91ff01d4f31a96b564ed42d9650e
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-18 13:42:53 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1588 ms, Memory: 76972 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -3,52 +3,10 @@ import sys
 
 def input():
     return sys.stdin.readline().strip()
-
-
-def m_sort(arr, start=0, end=False):
-    if end == False:
-        end = len(arr)
-    if end > start+1:
-        mid = (start+end)//2
-        m_sort(arr, start, mid)
-        m_sort(arr, mid, end)
-        merge(arr, start, mid, end)
-
-
-def merge(arr, start, mid, end):
-    lf = arr[start:mid]
-    rt = arr[mid:end]
-    now = start
-    li, ri = 0, 0
-    while li < len(lf) and ri < len(rt):
-        if lf[li] < rt[ri]:
-            arr[now] = lf[li]
-            li += 1
-        else:
-            arr[now] = rt[ri]
-            ri += 1
-        now += 1
-    while li < len(lf):
-        arr[now] = lf[li]
-        li += 1
-        now += 1
-    while ri < len(rt):
-        arr[now] = rt[ri]
-        ri += 1
-        now += 1
-    return
-
-
+mlist = []
 n = int(input())
-arr = []
-idx=1
+for _ in range(n):
+    mlist.append(int(input()))
+mlist.sort()
 for i in range(n):
-    arr.append(int(input()))
-for i in range(len(arr)-1):
-    if arr[i]>arr[i+1]:
-        idx+=1
-if idx==len(arr):
-    m_sort(arr)
-else: arr.sort()
-for i in range(len(arr)):
-    print(arr[i])
+    print(mlist[i])
