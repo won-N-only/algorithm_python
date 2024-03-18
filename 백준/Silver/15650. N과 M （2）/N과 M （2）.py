bb6a3ab3350d85233c0970c11f03c2d742369865
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-18 23:37:07 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,17 +1,16 @@
-def perm(arr, m):
-    res = []
-    if m == 0:
-        return [[]]
-    for i in range(len(arr)):
-        first = arr[i]
-        for rest in perm(arr[i+1:], m-1):
-            res.append([first]+rest)
-    return res
-
-
+ans = []
 n, m = map(int, input().split())
-arr = [i for i in range(1, n+1)]
 
-c = perm(arr, m)
-for i in range(len(c)):
-    print(*c[i])
+
+def btk2(z):
+    if m == len(ans):
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(z, n+1):
+        if i not in ans:
+            ans.append(i)
+            btk2(i+1)
+            ans.pop()
+
+
+btk2(1)
