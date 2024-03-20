05d7beeb0b8bc56c3ce9d34a607b9f7965d3c966
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-20 13:45:39 +0900

    [Gold IV] Title: 사냥꾼, Time: 40 ms, Memory: 31120 KB, Score: 23 point -BaekjoonHub

---
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def endangered(arr, lf, rt, obj):
+    mid = (lf+rt)//2
+    if rt <= lf:
+        return mid
+    if arr[mid] == obj:
+        return mid
+    elif arr[mid] > obj:
+        return endangered(arr, lf, mid-1, obj)
+    elif arr[mid] < obj:
+        return endangered(arr, mid+1, rt, obj)
+
+
+m, n, l = map(int, input().split())
+posi = [i for i in list(map(int, input().split()))]
+posi.sort()
+
+animal = []
+big_game = 0
+
+for i in range(n):
+    animal.append(list(map(int, input().split())))
+for i in range(n):
+    ex = endangered(posi, 0, m-1, animal[i][0])
+    if abs(posi[ex]-animal[i][0])+animal[i][1] <= l:
+        big_game += 1
+    else:
+        if ex+1 < m and abs(posi[ex+1]-animal[i][0])+animal[i][1] <= l:
+            big_game += 1
+
+print(big_game)
