427c09c56b2271d3ebc9079df7a48c8646dec94d
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-22 23:41:42 +0900

    [Gold IV] Title: 뱀, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -19,7 +19,7 @@ board = [[0]*n for _ in range(n)]
 k = int(input())
 for i in range(k):
     app, le = map(int, input().split())
-    #사과위치 -1안해줘서 idx error뜸
+    # 사과위치 -1안해줘서 idx error뜸
     board[app-1][le-1] = 1
 
 turn = []
@@ -39,7 +39,7 @@ snake = deque()
 snake.append([curr_x, curr_y])
 
 while True:
-    curr_x  += dx[dir]
+    curr_x += dx[dir]
     curr_y += dy[dir]
     time += 1
     # 판 나가거나 snake밟으면 컷
@@ -56,6 +56,8 @@ while True:
     # 시간되면 dir바꾸고
     if turn_idx < len(turn) and time == turn[turn_idx][0]:
         if turn[turn_idx][1] == 'L':
+            # abs때문에안됐네 .. 원래 abs(dir)이었음
+            # -1%4= 3.. 
             dir = (dir - 1) % 4
         else:
             dir = (dir + 1) % 4
