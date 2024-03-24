f373ec05fef3b490b41e7f55267089cc6955c08b
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-25 00:48:30 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -11,19 +11,21 @@ def dfs(x, y, arr):
     q = deque([(x, y)])
     arr[x][y] = 0
     dis = [[0]*m for i in range(n)]
-    dis[x][y] = 1
+    dis[x][y]=1
     while q:
         x, y = q.popleft()
+
         if (x, y) == (n-1, m-1):
             return dis[x][y]
+
         for i in range(4):
             px = x+dx[i]
             py = y+dy[i]
             if (0 <= px < n) and (0 <= py < m) and arr[px][py] == 1:
                 q.append((px, py))
+
                 # 도수분포표 활용함
                 arr[px][py] = 0
-
                 dis[px][py] = dis[x][y] + 1
     return dis[x][y]
 
@@ -33,6 +35,6 @@ n, m = map(int, input().split())
 maze = [[]for i in range(n)]
 for i in range(n):
     maze[i] = [int(i) for i in input()]
-end = (n, m)
+
 cnt = dfs(0, 0, maze)
 print(cnt)
