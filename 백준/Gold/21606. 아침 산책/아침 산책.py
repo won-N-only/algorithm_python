9d680972d551d089810dfc458ac51aca95a1d65c
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-25 15:57:06 +0900

    [Gold III] Title: 아침 산책, Time: 9800 ms, Memory: 67604 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,42 +1,29 @@
-import sys
-from collections import deque
-
-
-def input():
-    return sys.stdin.readline().strip()
-
-
-def dfs(start, arr):
-    stk = deque([start])
-    visit = set([start])
-    trail = 0
-
-    while stk:
-        curr = stk.pop()
-        for next in arr[curr]:
-            if next not in visit:
-                visit.add(next)
-                if a[next] == 0:
-                    stk.append(next)
-                elif a[next] == 1:
-                    trail += 1
-
-    return trail
-
+def dfs(start, graph, visited, indoor):
+    if visited[start]:  # 이미 방문한 노드는 탐색하지 않음
+        return 0
+    visited[start] = True
+    count = 0
+    for next in graph[start]:
+        if not visited[next]:
+            if indoor[next]:  # 다음 노드가 실내인 경우
+                count += 1  # 실내 장소에 도달한 경로를 카운트
+            else:
+                count += dfs(next, graph, visited, indoor)
+    visited[start] = False  # 다른 경로 탐색을 위해 방문 상태를 초기화
+    return count
 
 n = int(input())
-a = [0]+[int(char) for char in input()]
-route66 = [[] for i in range(n+1)]
-trail = 0
+a = [0] + [int(x) for x in input()]  # 실내/실외 정보
+graph = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-for i in range(n-1):
-    u, v = map(int, (input().split()))
-    route66[u].append(v)
-    route66[v].append(u)
-
-
+total_count = 0
+visited = [False] * (n+1)
 for i in range(1, n+1):
-    if a[i] == 1:
-        trail += dfs(i, route66)
+    if a[i] == 1:  # 실내에서 시작하는 모든 경로 탐색
+        total_count += dfs(i, graph, visited, a)
 
-print(trail)
+print(total_count)
