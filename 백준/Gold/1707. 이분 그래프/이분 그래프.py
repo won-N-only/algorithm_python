09510a4ef1a72ec70fe67f39d59e70ba65f3cccb
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-25 22:48:05 +0900

    [Gold IV] Title: 이분 그래프, Time: 1424 ms, Memory: 54352 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -16,10 +16,9 @@ def bfs(start, arr, visit):
         for next in arr[curr]:
             if colour[curr] == colour[next]:
                 return 1
-            if colour[curr] == 1:
-                colour[next] = 2
-            elif colour[curr] == 2:
-                colour[next] = 1
+            # 이부분 못고치나? 1,2대신 1,-1쓰면 curr, -curr로 쓸수도있을거같네
+            # 하노이처럼 3-curr해도 될듯?
+            colour[next] = (colour[curr]) % 2+1
             if next not in visit:
                 visit.add(next)
                 q.append(next)
@@ -30,10 +29,10 @@ k = int(input())
 
 for i in range(k):
     v, e = map(int, input().split())
-    res=0
+    res = 0
     # 정점수만큼 있어야하니까 정점기준
     graph = [[] for _ in range(v+1)]
-    colour = [[0] for _ in range(v+1)]
+    colour = [0] * (v+1)  # 원래 [0 for _ in range(v+1)] 였음
     visit = set([])
 
     for i in range(e):
@@ -42,10 +41,15 @@ for i in range(k):
         graph[b].append(a)
 
     # 연결안된 정점까지 방문
+    # 간소화 못하나? i부터 다 확인하니까 가성비안좋은데
+    # visit을 (unvisit)으로쓰면 될거같기도
+    # 0~v중에 방문한놈만빼고 not in 대신 in으로쓰면
+    # 안간곳 확인 빠를듯
+    # 방문때마다 list에서 remove해야하는데 O(n)은어케되지?
     for i in range(1, v+1):
-        if i not in visit:
+        if i not in visit and res == 0:
             res += bfs(i, graph, visit)
-    if res  >0:
+    if res > 0:
         print("NO")
     else:
         print("YES")
