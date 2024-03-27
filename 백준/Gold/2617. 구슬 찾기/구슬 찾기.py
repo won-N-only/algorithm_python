57ef50acc6e60ccaf9349815027df073ee900053
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-27 17:55:31 +0900

    [Gold IV] Title: 구슬 찾기, Time: 288 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def dfs(start, arr, find):
+    visit = set([(start)])
+    q = deque([(start)])
+    while q:
+        curr = q.pop()
+        for next in arr[curr]:
+            if next not in visit:
+                visit.add(next)
+                q.append(next)
+                find[start] += 1
+    return find[start]
+
+
+n, m = map(int, input().split())
+heavy_list = [[]for i in range(n+1)]
+light_list = [[]for i in range(n+1)]
+find_light = [0]*(n+1)
+find_heavy = [0]*(n+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    heavy_list[a].append(b)
+    light_list[b].append(a)
+
+for i in range(1, n+1):
+    dfs(i, light_list, find_light)
+    dfs(i, heavy_list, find_heavy)
+
+ans = 0
+for i in range(1, n+1):
+
+    if find_light[i] > n//2:
+        ans+=1
+    if find_heavy[i] > n//2:
+        ans+=1
+
+print(ans)
