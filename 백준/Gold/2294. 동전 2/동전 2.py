2ed67de9e2e4a26895b781c9e1de26bff1c277a7
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-27 20:30:54 +0900

    [Gold V] Title: 동전 2, Time: 800 ms, Memory: 121452 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,51 @@
+from collections import deque
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def bfs(start):
+    q = deque([(start, 1)])
+    visit = set([start])
+    while q:
+        ccost, cidx = q.popleft()
+
+        for next in coins:
+            if ccost+next not in visit:
+                if next > k or cidx > k:
+                    exit()
+
+                elif ccost+next < k:
+                    q.append((ccost+next, cidx+1))
+                    visit.add(ccost+next)
+
+                elif ccost+next == k:
+                    return cidx+1
+
+    return float('inf')
+
+
+n, k = map(int, input().split())
+
+coins = set()
+for i in range(n):
+    a = int(input())
+    if a < k:
+        coins.add(a)
+    elif a==k:
+        print(1)
+        exit()
+coins = sorted(list(coins), reverse=True)
+
+res = float('inf')
+
+for i in range(len(coins)):
+    kind = bfs(coins[i])
+    res = min(kind, res)
+
+if res != float('inf'):
+    print(res)
+else:
+    print(-1)
