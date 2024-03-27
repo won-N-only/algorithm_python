c3dfaf06ab0dd31e4c44d897f2e2f2468feeed63
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-27 18:46:53 +0900

    [Gold IV] Title: 구슬 찾기, Time: 260 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -1,40 +1,30 @@
 from collections import deque
 
-
-def dfs(start, arr, find):
-    visit = set([(start)])
-    q = deque([(start)])
+def dfs(start, graph):
+    visit = set([start])
+    q = deque([start])
+    count = 0
     while q:
         curr = q.pop()
-        for next in arr[curr]:
+        for next in graph[curr]:
             if next not in visit:
                 visit.add(next)
                 q.append(next)
-                find[start] += 1
-    return find[start]
-
+                count += 1
+    return count
 
 n, m = map(int, input().split())
-heavy_list = [[]for i in range(n+1)]
-light_list = [[]for i in range(n+1)]
-find_light = [0]*(n+1)
-find_heavy = [0]*(n+1)
+heavy_list = [[] for _ in range(n+1)]
+light_list = [[] for _ in range(n+1)]
 
-for i in range(m):
+for _ in range(m):
     a, b = map(int, input().split())
     heavy_list[a].append(b)
     light_list[b].append(a)
 
-for i in range(1, n+1):
-    dfs(i, light_list, find_light)
-    dfs(i, heavy_list, find_heavy)
-
 ans = 0
 for i in range(1, n+1):
-
-    if find_light[i] > n//2:
-        ans+=1
-    if find_heavy[i] > n//2:
-        ans+=1
+    if dfs(i, light_list) > n//2 or dfs(i, heavy_list) > n//2:
+        ans += 1
 
 print(ans)
