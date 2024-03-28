228d1c9a838b5b7ee895cf276a2ec022f14b3d18
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-28 17:46:51 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,24 @@
+def colour(n):
+    dp[1][1] = red[1]
+    dp[1][2] = blue[1]
+    dp[1][3] = green[1]
+    for i in range(2, n+1):
+        dp[i][1] = min(dp[i-1][2], dp[i-1][3])+red[i]
+        dp[i][2] = min(dp[i-1][1], dp[i-1][3])+blue[i]
+        dp[i][3] = min(dp[i-1][1], dp[i-1][2])+green[i]
+    return min(dp[n])
+
+
+n = int(input())
+red = [[0]]
+blue = [[0]]
+green = [[0]]
+dp = [[2e9]*(4) for i in range(n+1)]
+for i in range(n):
+    r, g, b = map(int, input().split())
+    red.append(r)
+    blue.append(b)
+    green.append(g)
+
+res = colour(n)
+print(res)
