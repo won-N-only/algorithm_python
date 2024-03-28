d2df497d78436f312f702688b779a8e6fd99db07
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-28 16:09:01 +0900

    [Silver III] Title: 1로 만들기, Time: 360 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+def dp_basic(n, d={}):
+    for i in range(2, n+1):
+        d[i] = d[i-1]+1
+        if i % 2 == 0:
+            d[i] = min(d[i], d[i//2]+1)
+        if i % 3 == 0:
+            d[i] = min(d[i], d[i//3]+1)
+
+    return print(d[n])
+
+
+n = int(input())
+d = [0]*(n+1)
+
+dp_basic(n, d)
