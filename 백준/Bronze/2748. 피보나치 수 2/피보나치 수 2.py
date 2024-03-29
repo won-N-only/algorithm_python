4da39b004c5d92cc9fefe0ee61ee18b4e5cb31c0
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-29 11:35:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,16 @@
+def pibo(n):
+
+    for i in range(3, n+1):
+        d[i] = d[i-1]+d[i-2]
+
+    return d[n]
+
+
+n = int(input())
+d = [0]*(n+1)
+if n > 2:
+    d[1] = d[2] = 1
+    ans = pibo(n)
+    print(ans)
+else:
+    print(1)
