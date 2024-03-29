3aecb861e11dc744b936390dda56050c6160340f
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-29 18:58:46 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 34768 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,15 +1,25 @@
 import re
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
 
 input_str = input()
-
 pattern = re.compile(r'[-+]?\d+')
 numbers = pattern.findall(input_str)
-numbers_int = [int(num) for num in numbers]
 
-idx = next((i for i, num in enumerate(
-    numbers_int) if num < 0), len(numbers_int))
+numbers_list = [int(num) for num in numbers]
 
-numbers_plus_sum = sum(numbers_int[0:idx])
-numbers_minus_sum = sum([-abs(i) for i in numbers_int[idx:]])
+
+idx = len(numbers_list)
+for i, num in enumerate(numbers_list):
+    if num < 0:
+        idx = i
+        break
+
+numbers_plus_sum = sum(numbers_list[0:idx])
+numbers_minus_sum = sum([-abs(i) for i in numbers_list[idx:]])
 
 print(numbers_plus_sum + numbers_minus_sum)
