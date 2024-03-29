244eac85ed22e392bd6bb70682310fa6409d5827
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-29 17:38:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,11 +1,23 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+    # 브루트포스말고 다른방법없나
+    # bfs로.. 는 같겠는데
+
+
+def find(n):
+    arr = list(map(int, input().split()))
+    dp = [0] * n
+
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)+1
+
+
 n = int(input())
-arr = list(map(int, input().split()))
-
-dp = [0] * n
-
-for i in range(1, n):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp)+1)
+print(find(n))
