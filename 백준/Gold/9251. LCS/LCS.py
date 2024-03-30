c3c17ec8c120f29680434cab8cd4b556a0f5685b
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-30 16:58:20 +0900

    [Gold V] Title: LCS, Time: 320 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,23 +1,18 @@
-import sys
-
-
-def input():
-    return sys.stdin.readline().strip()
-
-
-def correction(arr):
-    for i in range(1, len1+1):
-        for j in range(1, len2+1):
-            if s1[i-1] == s2[j-1]:
-                arr[i][j] += arr[i-1][j-1]+1
+def lcs_length(s1, s2):
+    len1, len2 = len(s1), len(s2)
+    dp = [0] * (len2 + 1)
+    
+    for i in range(1, len1 + 1):
+        prev = 0
+        for j in range(1, len2 + 1):
+            temp = dp[j]
+            if s1[i - 1] == s2[j - 1]:
+                dp[j] = prev + 1
             else:
-                arr[i][j] = max(arr[i-1][j], arr[i][j-1])
-    return print(arr[-1][-1])
-
+                dp[j] = max(dp[j], dp[j - 1])
+            prev = temp
+    return dp[len2]
 
 s1 = input()
-len1 = len(s1)
 s2 = input()
-len2 = len(s2)
-arr = [[0]*(len2+1)for _ in range(len1+1)]
-correction(arr)
+print( lcs_length(s1, s2))
