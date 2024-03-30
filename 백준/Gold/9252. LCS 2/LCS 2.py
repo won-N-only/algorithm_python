2b54bc2fb6cbc4971dce93a58fbb78f4d3d3f797
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-30 18:28:14 +0900

    [Gold IV] Title: LCS 2, Time: 356 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,32 +1,36 @@
-def find_lcs(a, b):
+def ans(a, b):
     dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
 
+    # LCS 길이 계산
     for i in range(1, len(a) + 1):
         for j in range(1, len(b) + 1):
-            if a[i - 1] == b[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
+    # LCS 추적
     i, j = len(a), len(b)
     lcs = []
-
     while i > 0 and j > 0:
-        if a[i - 1] == b[j - 1]:
-            lcs.append(a[i - 1])
+        if a[i-1] == b[j-1]:
+            lcs.append(a[i-1])
             i -= 1
             j -= 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
+        elif dp[i-1][j] > dp[i][j-1]:
             i -= 1
         else:
             j -= 1
 
-    lcs = ''.join(reversed(lcs))
-    print(dp[-1][-1])
-    print(lcs)
-    return
+    lcs.reverse()
+
+    print(len(lcs))
+    if lcs:
+        print(''.join(lcs))
 
 
+# 입력 받기
 a = input()
 b = input()
-find_lcs(a, b)
+
+ans(a, b)
