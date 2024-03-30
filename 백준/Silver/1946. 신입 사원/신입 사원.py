bc633653180df6d7a4a01dfad980fdbdbbec9591
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-30 20:32:15 +0900

    [Silver I] Title: 신입 사원, Time: 5216 ms, Memory: 72636 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,26 @@
+import heapq
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def interview(applicants):
+    applicants.sort()
+    idx = 1
+    best = applicants[0][1]
+
+    for _, score in applicants[1:]:
+        if score < best:
+            idx += 1
+            best = score
+
+    return idx
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    applicants = [list(map(int, input().split())) for _ in range(N)]
+    print(interview(applicants))
