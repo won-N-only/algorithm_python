6568c522c0789a1c1cefd25761c61da249988b1e
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-30 15:38:11 +0900

    [Gold IV] Title: LCS 2, Time: 368 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def correction(arr):
+    for i in range(1, len1+1):
+        for j in range(1, len2+1):
+            if s1[i-1] == s2[j-1]:
+                arr[i][j] += arr[i-1][j-1]+1
+            else:
+                arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+    return print(arr[-1][-1])
+
+
+def find_corr(arr):
+    i, j = len1, len2
+    ans = []
+
+    while i > 0 and j > 0:
+        if s1[i-1] == s2[j-1]:
+            ans.append(s1[i-1])
+            i -= 1
+            j -= 1
+        elif arr[i-1][j] == arr[i][j]:
+            i -= 1
+        elif arr[i][j] == arr[i][j-1]:
+            j -= 1
+    ans.reverse()
+    return print(''.join(ans))
+
+
+s1 = input()
+len1 = len(s1)
+s2 = input()
+len2 = len(s2)
+arr = [[0]*(len2+1)for _ in range(len1+1)]
+correction(arr)
+res = []
+find_corr(arr)
