d0a2845471b8aa79103d8223ee2e8f8420b55f93
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-30 15:50:49 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -1,18 +1,22 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
 def count_ways(coins, amounts, target):
     dp = [0] * (target + 1)
     dp[0] = 1
 
     for coin_idx, coin in enumerate(coins):
-        # 현재 동전을 1개부터 최대 amounts[coin_idx]개까지 사용하는 경우 고려
-        next_dp = dp[:]  # 현재까지의 dp 상태를 복사
+        next_dp = dp[:]
         for j in range(1, amounts[coin_idx] + 1):
             coin_value = coin * j
             for amount in range(coin_value, target + 1):
-                # 현재 동전을 사용해 amount 금액을 만드는 방법의 수 업데이트
                 next_dp[amount] += dp[amount - coin_value]
-        dp = next_dp  # dp 배열 업데이트
-
-    return dp[target]  # 목표 금액을 만들 수 있는 방법의 수 반환
+        dp = next_dp
+    return dp[target]
 
 
 n = int(input())
