fa14ee586634acc3f37334d59e2ce12428011e72
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-31 17:08:16 +0900

    [Gold IV] Title: 팰린드롬?, Time: 992 ms, Memory: 157272 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+def is_palindrome(a, b):
+    if a >= b:
+        return True
+    if dp[a][b] != -1:
+        return dp[a][b]
+    if num[a] == num[b] and is_palindrome(a + 1, b - 1):
+        dp[a][b] = 1
+    else:
+        dp[a][b] = 0
+    return dp[a][b]
+
+n = int(input())
+num = list(map(int, input().split()))
+m = int(input())
+dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(1 if is_palindrome(a-1, b-1) else 0)
