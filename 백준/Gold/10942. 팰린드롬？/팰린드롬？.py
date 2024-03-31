fccc6609e5f5dfd36973e3f1c9ca10109c8caa56
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-03-31 21:29:19 +0900

    [Gold IV] Title: 팰린드롬?, Time: 3272 ms, Memory: 62220 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,28 +1,31 @@
 import sys
+sys.setrecursionlimit(10**6)
 
 
 def input():
     return sys.stdin.readline().strip()
-def pelind(a, b):
-    key = a + b
-    if key in discriminator and discriminator[key] <= a:
-        return print(1)
 
-    original_a, original_b = a, b
-    while a < b:
-        if num[a] != num[b]:
-            return print(0)
-        a += 1
-        b -= 1
 
-    discriminator[key] = min(discriminator.get(key, float('inf')), original_a)
-    return print(1)
+def palin(a, b):
+    if a >= b:
+        return 1
+
+    if dp[a][b] != -1:
+        return dp[a][b]
+    
+    if num[a] == num[b]:
+        dp[a][b] = palin(a + 1, b - 1)
+    else:
+        dp[a][b] = 0
+
+    return dp[a][b]
+
 
 n = int(input())
 num = list(map(int, input().split()))
 m = int(input())
-discriminator = {}
+dp = [[-1 for _ in range(n)] for _ in range(n)]
 
-for i in range(m):
+for _ in range(m):
     a, b = map(int, input().split())
-    pelind(a-1, b-1)
+    print(1 if palin(a-1, b-1) else 0)
