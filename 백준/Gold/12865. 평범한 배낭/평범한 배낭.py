d867840f42a33f3bfa1191a6a559be98fe30b4e4
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-01 20:26:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 2064 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -5,28 +5,27 @@ def input():
     return sys.stdin.readline().strip()
 
 
-def mybag(n, k, arr):
-    for i in range(1, n+1):
-        for w in range(1, k+1):
-            if arr[i-1][0] <= w:
-                dp[i][w] = max(dp[i-1][w], dp[i-1][w-arr[i-1][0]]+arr[i-1][1])
-            else:
-                dp[i][w] = dp[i-1][w]
-    return dp[n][k]
 
+
+def my_wallet(k, arr):
+    dp2 = [0] * (k+1)
+
+    for wei, v in arr:
+        for w in range(k, wei-1, -1):
+            dp2[w] = max(dp2[w], dp2[w-wei] + v)
+    return max(dp2)
 
 
 n, k = map(int, input().split())
 # 이거 dp = [[0]*(k+1)]*(n+1)이렇게만들면 [0]이 다 같은 곳을 참조해서 안됨
-dp = [[0]*(k+1)for _ in range(n+1)]
 
 not_in_bag = []
 for _ in range(n):
     a, b = map(int, input().split())
     not_in_bag.append([a, b])
-
 not_in_bag.sort()
 
-ans = mybag(n, k, not_in_bag)
 
-print(ans)
+ans2 = my_wallet(k, not_in_bag)
+
+print(ans2)
