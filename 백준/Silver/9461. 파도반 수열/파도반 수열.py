32bf688714c44d579244e78e06d87ca21ac93ff1
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-03 16:00:24 +0900

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,13 @@
+t = int(input())
+triangle = [0 for i in range(101)]
+triangle[1] = 1
+triangle[2] = 1
+triangle[3] = 1
+triangle[4] = 2
+triangle[5] = 2
+for i in range(6, 101):
+    triangle[i] = triangle[i-5]+triangle[i-1]
+
+
+for i in range(t):
+    print(triangle[int(input())])
