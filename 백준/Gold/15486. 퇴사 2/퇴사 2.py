72c305dd3f47699c9f31b5b8c96d2c4807f169dd
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-03 19:26:41 +0900

    [Gold V] Title: 퇴사 2, Time: 636 ms, Memory: 217580 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,25 +1,17 @@
 import sys
 
-
-def input():
-    return sys.stdin.readline().strip()
-
-
-def quit():
-    for i in range(n):
-        if i+T[i] <= n:
-            dp[i+T[i]] = max(dp[i+T[i]], dp[i]+P[i])
-        dp[i+1] = max(dp[i+1], dp[i])
-    return dp
-
-
-n = int(input())
+n = int(sys.stdin.readline())
 T = []
 P = []
 dp = [0]*(n+1)
-for i in range(n):
-    t, p = map(int, input().split())
+for _ in range(n):
+    t, p = map(int, sys.stdin.readline().split())
     T.append(t)
     P.append(p)
-ans = quit()
-print(ans[-1])
+
+for i in range(n):
+    if i+T[i] <= n:
+        dp[i+T[i]] = max(dp[i+T[i]], dp[i]+P[i])
+    dp[i+1] = max(dp[i+1], dp[i])
+
+print(dp[-1])
