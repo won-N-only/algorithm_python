34039fa34cd9c2d044a455f9e1b391b2c4652166
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-03 19:46:01 +0900

    [Silver II] Title: 제곱수의 합, Time: 2420 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def square(n):
+    for i in range(1, n+1):
+        dp[i] = i
+        j = 1
+        while j*j <= i:
+            if dp[i] > dp[i-j*j]+1:
+                dp[i] = dp[i-j*j]+1
+            j += 1
+    return dp[n]
+
+
+n = int(input())
+dp = [0]*(n+1)
+ans=square(n)
+print(ans)
