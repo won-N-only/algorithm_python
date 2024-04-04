7c9b90f761eb9e2c25e99e66e3acf5dd6c14eaf5
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-04 18:50:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long a;
+    long b;
+    long c;
+    long ans;
+    scanf("%ld %ld %ld", &a, &b, &c);
+    ans = a + b + c;
+    printf("%ld", ans);
+}
