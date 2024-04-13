c38d059b295fc4209d08df13d84273697e8b73c7
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-13 19:58:09 +0900

    [Silver II] Title: 초콜릿 식사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.py
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.py
@@ -11,31 +11,20 @@ def power(k):
             return 2**i
 
 
-def choco(k):
+def choco(k, res):
     cnt = 0
-    last_chocolate = 1
-    while k > 0:
-        last_chocolate = power(k) // 2
-        cnt += 1
-        if k - last_chocolate >= 0:
-            k -= last_chocolate
-    return cnt
+    if k != res:
+        while k > 0:
+            res //= 2
+            if k >= res:
+                k = k - res
+                cnt += 1
+            else:
+                cnt += 1
+    print(cnt)
 
-
-def choco(k):
-    cnt = 0  
-    temp = res  
-    if k != res:  
-        while k :  
-            temp //= 2 
-            if k >= temp:  
-                k = k - temp  
-                cnt += 1  
-            else:  
-                cnt += 1 
-    print(res, cnt)
 
 k = int(input())
 res = power(k)
-
-choco(k)
+print(res, end=" ")
+choco(k, res)
