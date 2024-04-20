1e5cb745bc40dc34cda0550a2f20982d8b2ba986
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-04-20 20:08:52 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,19 @@
+import sys
+def input(): return sys.stdin.readline().strip()
+
+
+n, m = map(int, input().split())
+medals=[]
+
+for i in range(n):
+    medals.append(list(map(int, input().split())))
+
+medals.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+
+for j in range(n):
+    if medals[j][0] == m:
+        idx = j
+for i in range(n):
+    if medals[idx][1:]==medals[i][1:]:
+        print(i+1)
+        break
