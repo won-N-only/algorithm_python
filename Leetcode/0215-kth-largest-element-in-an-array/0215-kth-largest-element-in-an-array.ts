313af0079406fe85664d2d8709a7092d24404758
won-N-only <162883112+won-N-only@users.noreply.github.com>
2024-05-31 23:04:00 +0900

    Time: 95 ms (83.95%), Space: 58.6 MB (99.28%) - LeetHub

--- Leetcode/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.ts
+++ Leetcode/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.ts
@@ -1,15 +1,26 @@
 function findKthLargest(nums: number[], k: number): number {
-  //   let pivot: number = 0; //인덱스로 가져가
-  //   let temp: number[] = [];
+  //1차 풀이 일단 정렬해
+  //   nums.sort((a, b) => a - b);
+  //   return nums[k - 1];
 
-  //   pivot = nums[pivot];
-  //   temp.push(nums.pop() ?? 0);
-  //   for (let i: number = 0; i < nums.length; i++) {
-  //     if (pivot < nums[i]) temp.push(nums[i]);
-  //     if (pivot >= nums[i]) temp.unshift(nums[i]);
-  //   }
+  //2차 풀이 이분탐색
+  /* l,r을 inf로 넣어버리니까 m이 NaN이 됨.. 어짬  
+  -10^4 <= nums[i] <= 10^4 이니까 맞춰서 설정함 */
+  let [l, r]: number[] = [-(10 ** 4) - 1, 10 ** 4 + 1];
 
-  //   return 3;
-  nums.sort((a, b) => b-a);
-  return nums[k - 1];
+  const getCount = (m: number) => {
+    let count: number = 0;
+    for (const n of nums) {
+      if (n >= m) count++;
+    }
+    return count;
+  };
+
+  while (l < r) {
+    const m: number = Math.round((r + l) / 2);
+    if (getCount(m) >= k) l = m;
+    else r = m - 1;
+  }
+
+  return l;
 }
