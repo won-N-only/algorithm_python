3ab038da892d1fa2abbd4957858d5fcb86ceb815
JW.Kim <162883112+won-N-only@users.noreply.github.com>
2024-09-15 22:46:41 +0900

    [Silver IV] Title: 큐, Time: 60 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+
+def input():
+    return sys.stdin.readline()
+
+
+N = int(input())
+
+dq = deque()
+
+for i in range(N):
+    cmd = input().split()
+    if cmd[0] == 'push':
+        dq.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if len(dq) != 0:
+            print(dq.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(dq))
+    elif cmd[0] == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'front':
+        if len(dq) != 0:
+            print(dq[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if len(dq) != 0:
+            print(dq[-1])
+        else:
+            print(-1)
