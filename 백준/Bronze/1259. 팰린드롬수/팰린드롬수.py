fc8657122452b484c99672830632fd1c9c7015e7
JW.Kim <162883112+won-N-only@users.noreply.github.com>
2024-09-15 22:29:04 +0900

    [Bronze I] Title: 팰린드롬수, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline()
+
+
+while 1:
+    n = input().strip()
+    if n == "0":
+        break
+    if n == n[::-1]:
+        print("yes")
+    else:
+        print("no")
