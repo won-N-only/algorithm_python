ea777e7f5990798687c5a4e4b08351068e485c88
JW.Kim <162883112+won-N-only@users.noreply.github.com>
2024-09-21 22:01:20 +0900

    [Silver III] Title: 소수 구하기, Time: 164 ms, Memory: 41600 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,26 +1,25 @@
 import sys
 
-
 def input():
     return sys.stdin.readline().strip()
 
-
 def era(limit):
     sieve = [True] * (limit + 1)
-    sieve[0], sieve[1] = False, False
+    sieve[0], sieve[1] = False, False  
 
     for i in range(2, int(limit**0.5) + 1):
-        if sieve[i]:
-            for j in range(i*i, limit + 1, i):
+        if sieve[i]:  
+            for j in range(i * i, limit + 1, i): 
                 sieve[j] = False
 
-    return [i for i in range(2, limit + 1) if sieve[i]]
-
+    return [i for i in range(2, limit + 1) if sieve[i]] 
 
 limit = 10**6
-primes = era(limit)
+primes = era(limit)  
 n, m = map(int, input().split())
 
 for prime in primes:
-    if prime >= n and prime < m+1:
+    if prime > m: 
+        break
+    if prime >= n:  
         print(prime)
