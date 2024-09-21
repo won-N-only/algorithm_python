091306bf0929d1dfc8d423aa0a76714f1d093ae2
JW.Kim <162883112+won-N-only@users.noreply.github.com>
2024-09-21 21:59:42 +0900

    [Silver III] Title: 소수 구하기, Time: 140 ms, Memory: 123168 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip()
+
+
+def era(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0], sieve[1] = False, False
+
+    for i in range(2, int(limit**0.5) + 1):
+        if sieve[i]:
+            for j in range(i*i, limit + 1, i):
+                sieve[j] = False
+
+    return [i for i in range(2, limit + 1) if sieve[i]]
+
+
+limit = 10**6
+primes = era(limit)
+n, m = map(int, input().split())
+
+for prime in primes:
+    if prime >= n and prime < m+1:
+        print(prime)
